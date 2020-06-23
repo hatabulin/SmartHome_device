889da4ee0f66071b1027e929396824eda7429629
Sergey <33245236+hatabulin@users.noreply.github.com>
2020-06-23 08:30:02 +0300

    Add files via upload

---
+++ Src/hex_utils.c
@@ -0,0 +1,39 @@
+/*
+ * hex_utils.c
+ *
+ *  Created on: 22 θών. 2020 γ.
+ *      Author: User
+ */
+#include "stdio.h"
+
+uint32_t hex2int(char *hex) {
+    uint32_t val = 0;
+    while (*hex) {
+        // get current character then increment
+        uint8_t byte = *hex++;
+        // transform hex character to the 4bit equivalent number, using the ascii table indexes
+        if (byte >= '0' && byte <= '9') byte = byte - '0';
+        else if (byte >= 'a' && byte <='f') byte = byte - 'a' + 10;
+        else if (byte >= 'A' && byte <='F') byte = byte - 'A' + 10;
+        // shift 4 to make space for new digit, and add the 4 bits of the new digit
+        val = (val << 4) | (byte & 0xF);
+    }
+    return val;
+}
+
+uint8_t hex2int_byte(char *hex) {
+    uint32_t val = 0;
+    uint8_t i=0;
+    while (i<2){
+        // get current character then increment
+        uint8_t byte = *hex++;
+        i++;
+        // transform hex character to the 4bit equivalent number, using the ascii table indexes
+        if (byte >= '0' && byte <= '9') byte = byte - '0';
+        else if (byte >= 'a' && byte <='f') byte = byte - 'a' + 10;
+        else if (byte >= 'A' && byte <='F') byte = byte - 'A' + 10;
+        // shift 4 to make space for new digit, and add the 4 bits of the new digit
+        val = (val << 4) | (byte & 0xF);
+    }
+    return val;
+}
