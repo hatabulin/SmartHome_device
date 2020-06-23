b77180998207a70de083d12a4b04844231402248
Sergey <33245236+hatabulin@users.noreply.github.com>
2020-06-23 08:30:26 +0300

    Add files via upload

---
+++ Inc/hex_utils.h
@@ -0,0 +1,16 @@
+/*
+ * hex_utils.h
+ *
+ *  Created on: 22 θών. 2020 γ.
+ *      Author: User
+ */
+
+#ifndef HEX_UTILS_H_
+#define HEX_UTILS_H_
+
+
+
+#endif /* HEX_UTILS_H_ */
+
+uint32_t hex2int(char *hex);
+uint32_t hex2int_byte(char *hex);
